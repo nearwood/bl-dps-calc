2f73f3c4eb6d79e979117a25ad9db7abac456d19
Nick <nearwood@gmail.com>
2019-11-20 15:17:05 -0600

    Fix styling of selects and fonts

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
         <table>
           <tbody>
             <tr><td><label htmlFor="damage">Damage</label></td>
-              <td><input onFocus={selectAll} onChange={handler} name="damage" type="number" value={form.damage.toString()} min="0"/>
+              <td><input onFocus={selectAll} onChange={handler} name="damage" type="number" className="shrink" value={form.damage.toString()} min="0"/>
               <select onChange={handler} name="damageMultiplier" id="damageMultiplier" title="Damage Multiplier" value={form.damageMultiplier}>
                 <option value="1">x1</option>
                 <option value="2">x2</option>
@@ -79,7 +79,7 @@ function App() {
             <tr><td><label htmlFor="reloadTime">Reload Time</label></td><td><input onFocus={selectAll} onChange={handler} name="reloadTime" type="number" value={form.reloadTime.toString()} min="0" step="0.1"/></td></tr>
             <tr><td><label htmlFor="fireRate">Fire Rate</label></td><td><input onFocus={selectAll} onChange={handler} name="fireRate" type="number" value={form.fireRate.toString()} min="0" step="0.01"/></td></tr>
             <tr><td><label htmlFor="magazineSize">Magazine Size</label></td>
-              <td><input onFocus={selectAll} onChange={handler} name="magazineSize" type="number" value={form.magazineSize.toString()} min="0"/>
+              <td><input onFocus={selectAll} onChange={handler} name="magazineSize" type="number" className="shrink" value={form.magazineSize.toString()} min="0"/>
               <select onChange={handler} name="bulletsPerShot" id="bulletsPerShot" title="Bullets per shot" value={form.bulletsPerShot}>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -101,11 +101,11 @@ function App() {
     return (
       <table>
         <tbody>
-        <tr><td>Mag-2-Mag DPS:</td><td>{m2mDps}</td>{gunA && percentDelta(gunA.mag2magDps, gun.mag2magDps)}</tr>
-        <tr><td>Base DPS:</td><td>{dps}</td>{gunA && percentDelta(gunA.dps, gun.dps)}</tr>
-        {gunA && <tr><td>Fire Rate:</td><td>{(gun.fireRate - gunA.fireRate).toFixed(2)}</td>{percentDelta(gunA.fireRate, gun.fireRate)}</tr>}
-        {gunA && <tr><td>Reload time:</td><td>{(gun.reloadTime - gunA.reloadTime).toFixed(2)}</td>{percentDelta(gunA.reloadTime, gun.reloadTime, true)}</tr>}
-        {gunA && <tr><td>Magazine Size:</td><td>{(gun.magazineSize - gunA.magazineSize).toFixed(2)}</td>{percentDelta(gunA.magazineSize, gun.magazineSize)}</tr>}
+          <tr><th>Effective DPS:</th><th>{m2mDps}</th>{gunA && percentDelta(gunA.mag2magDps, gun.mag2magDps)}</tr>
+          <tr><td>Base DPS:</td><td>{dps}</td>{gunA && percentDelta(gunA.dps, gun.dps)}</tr>
+          {gunA && <tr><td>Fire Rate:</td><td>{(gun.fireRate - gunA.fireRate).toFixed(2)}</td>{percentDelta(gunA.fireRate, gun.fireRate)}</tr>}
+          {gunA && <tr><td>Reload time:</td><td>{(gun.reloadTime - gunA.reloadTime).toFixed(2)}</td>{percentDelta(gunA.reloadTime, gun.reloadTime, true)}</tr>}
+          {gunA && <tr><td>Magazine Size:</td><td>{(gun.magazineSize - gunA.magazineSize).toFixed(2)}</td>{percentDelta(gunA.magazineSize, gun.magazineSize)}</tr>}
         </tbody>
       </table>
     );
@@ -127,8 +127,8 @@ function App() {
               <span>Gun A</span>
               {renderForm(formA, handlerA)}
             </div>
+            <hr/>
             <div className="flex flexColumn">
-              <h2>Stats</h2>
               {renderStats(gunA)}
             </div>
           </TabPanel>
@@ -137,8 +137,8 @@ function App() {
               <span>Gun B</span>
               {renderForm(formB, handlerB)}
             </div>
+            <hr/>
             <div className="flex flexColumn">
-              <h2>Stats</h2>
               {renderStats(gunB, gunA)}
             </div>
           </TabPanel>
