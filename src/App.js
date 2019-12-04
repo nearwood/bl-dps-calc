19c1b513df4fb85b8ca614b48d7e9d4a86f02b17
Nick <nearwood@gmail.com>
2019-12-04 08:47:29 -0600

    Fix tabindex of selects

--- src/App.js
+++ src/App.js
@@ -65,8 +65,8 @@ function App() {
         <table>
           <tbody>
             <tr><td><label htmlFor="damage">Damage</label></td>
-              <td><input onFocus={selectAll} onChange={handler} name="damage" type="number" className="shrink" value={form.damage.toString()} min="0"/>
-              <select onChange={handler} name="damageMultiplier" id="damageMultiplier" title="Damage Multiplier" value={form.damageMultiplier}>
+              <td><input tabindex="1" onFocus={selectAll} onChange={handler} name="damage" type="number" className="shrink" value={form.damage.toString()} min="0"/>
+              <select tabindex="2" onChange={handler} name="damageMultiplier" id="damageMultiplier" title="Damage Multiplier" value={form.damageMultiplier}>
                 <option value="1">x1</option>
                 <option value="2">x2</option>
                 <option value="3">x3</option>
@@ -76,11 +76,11 @@ function App() {
             </td></tr>
             {/*<tr><td><label htmlFor="accuracy">Accuracy</label></td><td><input disabled onChange={handler} name="accuracy" type="number" value={form.accuracy.toString()} min="0" max="100"/></td></tr>*/}
             {/*<tr><td><label htmlFor="handling">Handling</label></td><td><input onChange={handler} name="handling" type="number" value="" min="0" max="100"/></td></tr>*/}
-            <tr><td><label htmlFor="reloadTime">Reload Time</label></td><td><input onFocus={selectAll} onChange={handler} name="reloadTime" type="number" value={form.reloadTime.toString()} min="0" step="0.1"/></td></tr>
-            <tr><td><label htmlFor="fireRate">Fire Rate</label></td><td><input onFocus={selectAll} onChange={handler} name="fireRate" type="number" value={form.fireRate.toString()} min="0" step="0.01"/></td></tr>
+            <tr><td><label htmlFor="reloadTime">Reload Time</label></td><td><input tabindex="1" onFocus={selectAll} onChange={handler} name="reloadTime" type="number" value={form.reloadTime.toString()} min="0" step="0.1"/></td></tr>
+            <tr><td><label htmlFor="fireRate">Fire Rate</label></td><td><input tabindex="1" onFocus={selectAll} onChange={handler} name="fireRate" type="number" value={form.fireRate.toString()} min="0" step="0.01"/></td></tr>
             <tr><td><label htmlFor="magazineSize">Magazine Size</label></td>
-              <td><input onFocus={selectAll} onChange={handler} name="magazineSize" type="number" className="shrink" value={form.magazineSize.toString()} min="0"/>
-              <select onChange={handler} name="bulletsPerShot" id="bulletsPerShot" title="Bullets per shot" value={form.bulletsPerShot}>
+              <td><input tabindex="1" onFocus={selectAll} onChange={handler} name="magazineSize" type="number" className="shrink" value={form.magazineSize.toString()} min="0"/>
+              <select tabindex="2" onChange={handler} name="bulletsPerShot" id="bulletsPerShot" title="Bullets per shot" value={form.bulletsPerShot}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
